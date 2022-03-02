d307aa2a1d9743a0eb6d3c58fb1713c638078cd5
gacon91hp <nduc0886@gmail.com>
2022-03-02 17:42:35 +0700

    delete

--- src/main/java/com/example/baitap/BaitapApplication.java
+++ src/main/java/com/example/baitap/BaitapApplication.java
@@ -1,7 +1,5 @@
 package com.example.baitap;
 
-import com.example.baitap.component.Dongvat;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,8 +9,6 @@ public class BaitapApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(BaitapApplication.class, args);
-		Running running = context.getBean(Running.class);
-		System.out.printf(String.valueOf(running.test));
 
 	}
 
--- src/main/java/com/example/baitap/component/Item.java
+++
@@ -1,8 +0,0 @@
-package com.example.baitap.component;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface Item {
-    String itemOption();
-}
